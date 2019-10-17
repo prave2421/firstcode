5e6fb9998b1b15407cc39df0060f8d7473d9a678
prave2421 <praveen,ch2000@gmail.com>
2019-10-17 17:26:39 -0500

    all new changes done

---
+++ mail_alert.py
@@ -0,0 +1,16 @@
+"""The first step is to create an SMTP object, each object is used for connection 
+with one server."""
+"""
+git learning 
+"""
+import smtplib
+server = smtplib.SMTP('smtp.gmail.com', 587)
+#new line 2 added
+#Next, log in to the server
+#ne wline added
+server.login("youremailusername", "password")
+
+#Send the mail
+msg = "
+Hello!" # The /n separates the message from the headers
+server.sendmail("you@gmail.com", "target@example.com", msg)
